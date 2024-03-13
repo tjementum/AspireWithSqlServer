d5b175466273e0ba2769ec9f2e43a99707c8a37f
Thomas Jespersen <tje@platformplatform.net>
2024-03-13 11:57:48 +0100

    Disable EF Core tracking... this only works when connection DB Context connection pppling is disable

--- WebApi/Persistence/WeatherForecastContext.cs
+++ WebApi/Persistence/WeatherForecastContext.cs
@@ -5,4 +5,11 @@ namespace AspireWithSqlServer.WebApi.Persistence;
 public class WeatherForecastContext(DbContextOptions<WeatherForecastContext> options) : DbContext(options)
 {
     public DbSet<WeatherForecast> WeatherForecasts => Set<WeatherForecast>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+
+        base.OnConfiguring(optionsBuilder);
+    }
 }
